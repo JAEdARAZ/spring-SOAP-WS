090424d5dc9c6302cd7fb39b49c850af4d7ae755
Jaime Aranzazu <jaedaraz@gmail.com>
2021-02-14 13:29:58 +0100

    First commit

---
+++ src/main/java/com/jaedaraz/soapws/PracticeSoapWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.jaedaraz.soapws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSoapWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeSoapWebServicesApplication.class, args);
+	}
+
+}
